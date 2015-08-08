419457abcc438315b34820fe770518ab1b74c870
Jonathan Raphaelson <jon@accidental.cc>
2015-08-08 14:07:45 -0600

    changes

--- customize.el
+++ customize.el
@@ -11,4 +11,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:background "#263238" :foreground "#ffffff" :height 80 :family "Fira Mono")))))
+ '(default ((t (:inherit nil :stipple nil :background "#263238" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :width normal :foundry "unknown" :family "Fira Mono" :pixels 9)))))
