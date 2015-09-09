cea86470a465bfb5cdf8dad44c36b2976a9dc608
Jonathan Raphaelson <jon@accidental.cc>
2015-09-09 01:01:47 -0600

    modeline and flycheck

--- init.el
+++ init.el
@@ -1,3 +1,8 @@
+;;; init.el --- my initialization stuff
+;;; Commentary:
+;;; get er' done
+
+;;; Code:
 ;; initialize elpa
 (require 'package)
 (add-to-list 'package-archives
@@ -9,11 +14,11 @@
 ;; bootstrapping configuration
 (defvar init-source-org-file
   (expand-file-name "emacs.org" user-emacs-directory)
-  "The file that our emacs configuration comes from.")
+  "The file that our Emacs configuration comes from.")
 
 (defvar init-source-el-file
   (expand-file-name "emacs.el" user-emacs-directory)
-  "The file that our emacs configuration is generated into.")
+  "The file that our Emacs configuration is generated into.")
 
 ;; load the org file iff it's newer than the generated el file
 (if (file-exists-p init-source-org-file)
@@ -22,4 +27,22 @@
 	(load-file init-source-el-file)
       (progn (require 'org)
 	     (org-babel-load-file init-source-org-file)))
-  (error "'%s' file is missing." init-source-org-file))
+  (error "'%s' file is missing" init-source-org-file))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(provide 'init)
+;;; init.el ends here
