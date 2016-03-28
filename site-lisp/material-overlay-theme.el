93740e6d3704fd350b388a242b63178c456a4520
Jonathan Raphaelson <jon@accidental.cc>
2016-03-27 23:00:08 -0600

    add font to overlay

--- site-lisp/material-overlay-theme.el
+++ site-lisp/material-overlay-theme.el
@@ -6,6 +6,7 @@
 
 (custom-theme-set-faces
  'material-overlay
+ '(default           ((t . (:family "Fira Mono" :height 97))))
  '(linum             ((t . (:height 0.8 :foreground "#535a6a"))))
  '(rel-linum         ((t . (:inherit (linum) :foreground "#5d6e92"))))
  '(rel-linum-current ((t . (:inherit (linum) :foreground "#587cca")))))
